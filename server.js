e33beb5fcb5aa3d44c1ff88fbdcd82f7ae4059c1
Legacynical <legacynical@gmail.com>
2024-04-06 09:58:24 -0700

    Complete request.html

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+
+const app = express();
+
+// Set static folder
+app.use(express.static('public'));
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// handle GET request to fetch users
+app.get('/users', async (req, res) => {
+  // const users = [
+  //   {id: 1, name: 'Bruce Wayne'},
+  //   {id: 1, name: 'Tony Stark'},
+  //   {id: 1, name: 'Keanu Reeves'}
+  // ];
+
+  setTimeout(async () => {    
+    const limit = +req.query.limit || 10;
+    
+    const response = await fetch(`https:jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
+    const users = await response.json();
+  
+    res.send(`
+      <h1 class="text-2xl font-bold my-4">Users</h1>
+      <ul>
+        ${users.map((user) => `<li>${user.name}</li>`).join('')}
+      </ul>
+    `);
+  }, 1000);
+
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
