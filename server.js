fd6dec0c0fdb2ce1f01eea52c7e11b9440f50413
Legacynical <legacynical@gmail.com>
2024-04-10 23:57:44 -0700

    Complete search.html

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/users', async (req, res) => {
   setTimeout(async () => {    
     const limit = +req.query.limit || 10;
     
-    const response = await fetch(`https:jsonplaceholder.typicode.com/users?_limit=${limit}`
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`
     );
     const users = await response.json();
   
@@ -66,6 +66,76 @@ let currentTemperature = 20;
 app.get('/get-temperature', (req, res) => {
   currentTemperature += Math.random() * 2 - 1; // Random temp change
   res.send(currentTemperature.toFixed(1) + '℃');
+});
+
+const contacts = [
+  { name: 'Bruce Wayne', email: 'dark_knight@gotham.com' },
+  { name: 'Tony Stark', email: 'ily3000@avengers.com' },
+  { name: 'Clark Kent', email: '2020vision@dailyplanet.com' },
+  { name: 'Rick Sanchez', email: 'getschwifty@c137.com' },
+  { name: 'Sherlock Holmes', email: 'elementary@221bbakerst.com' },
+  { name: 'Spongebob Squarepants', email: 'frycook@bikinibottom.com' },
+];
+
+// Handle POST request for contacts search
+app.post('/search', (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send('<tr></tr>');
+  }
+
+  const searchResults = contacts.filter((contact) => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+
+    return name.includes(searchTerm) || email.includes(searchTerm);
+  });
+
+  setTimeout(() => {
+    const searchResultHtml = searchResults
+      .map(
+        (contact) => `
+      <tr>
+        <td><div class="my-4 p-2">${contact.name}</div></td>
+        <td><div class="my-4 p-2">${contact.email}</div></td>
+      </tr>
+    `
+      )
+      .join('');
+
+    res.send(searchResultHtml);
+  }, 1000);
+});
+
+// Handle POST request for contacts search from jsonplaceholder
+app.post('/search/api', async (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send('<tr></tr>');
+  }
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const contacts = await response.json();
+
+  const searchResults = contacts.filter(contact => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+
+    return name.includes(searchTerm) || email.includes(searchTerm);
+  });
+
+  setTimeout(() => { 
+    const searchResultHtml = searchResults.map(contact => `
+      <tr>
+        <td><div class="my-4 p-2">${contact.name}</div></td>
+        <td><div class="my-4 p-2">${contact.email}</div></td>
+      </tr>
+    `).join('');
+
+    res.send(searchResultHtml);
+  }, 1000);
 });
 
 // Start the server
